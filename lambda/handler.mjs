53b2fca3ee6bee40f9e174ea3c526f17a17388ec
Le Quang Thang <lethang7794@gmail.com>
2025-06-10 16:53:57 +0700

    Update Lambda function with CDK Pipelines

--- lambda/handler.mjs
+++ lambda/handler.mjs
@@ -1,8 +1,7 @@
 export async function handler(event) {
-  console.log("request:", JSON.stringify(event, undefined, 2));
-  throw new Error("Simulated error");
+  console.log('request:', JSON.stringify(event, undefined, 2));
   return {
     statusCode: 200,
-    body: `Hello from CDK v2! You've hit ${event.path}\n`,
+    body: `Hello from CDK v2 and CDK Pipelines! You've hit ${event.path}\n`,
   };
 }
