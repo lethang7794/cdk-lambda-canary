06223d357dfd7439f0dc98689106bf1be885e002
Le Quang Thang <lethang7794@gmail.com>
2025-06-13 00:53:32 +0700

    updating the lambda code with AWS CodePipeline

--- lambda/handler.mjs
+++ lambda/handler.mjs
@@ -2,6 +2,6 @@ export async function handler(event) {
   console.log('request:', JSON.stringify(event, undefined, 2));
   return {
     statusCode: 200,
-    body: `Hello from CDK v2! You've hit ${event.path}\n`,
+    body: `Hello from CDK v2 and AWS CodePipeline! You've hit ${event.path}\n`,
   };
 }
