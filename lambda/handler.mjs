a174cf88cd6a43037c7c6a25641a2c3ee100ab73
Le Quang Thang <lethang7794@gmail.com>
2025-06-12 16:59:32 +0700

    remove simulated error

--- lambda/handler.mjs
+++ lambda/handler.mjs
@@ -1,6 +1,5 @@
 export async function handler(event) {
-  console.log("request:", JSON.stringify(event, undefined, 2));
-  throw new Error("Simulated error");
+  console.log('request:', JSON.stringify(event, undefined, 2));
   return {
     statusCode: 200,
     body: `Hello from CDK v2! You've hit ${event.path}\n`,
